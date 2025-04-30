e2c64d19714365a980545ecf861fe8bfa7242096
Iryna Kopchak <iryna.kopchak39@gmail.com>
2025-04-30 16:21:56 +0300

    Add api gateway

---
+++ api-gateway/src/main/java/io/github/solenyk/api/gateway/ApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package io.github.solenyk.api.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+}
